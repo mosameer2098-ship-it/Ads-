9fa68caaf0856a4cca8d470f83f92392822a2d28
mosameer2098-ship-it <mosameer2098@gmail.com>
2026-08-10 00:14:22 +0530

    Update config.py

--- ads-premium/config.py
+++ ads-premium/config.py
@@ -1,1 +1,16 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+
+# Tumhare Telegram channel ka username
+# Example: @mychannel
+CHANNEL_USERNAME = os.getenv("CHANNEL_USERNAME", "")
+
+# Tumhari Telegram numeric ID
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+# Premium price
+PREMIUM_PRICE = 400
